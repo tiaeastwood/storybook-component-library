134119e1fd9a8aa997e535e629d9d726b3aff8da
Tia Eastwood <tia.eastwood@ii.co.uk>
2025-06-30 16:14:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### Storybook Component Library
+
+This project is an example of a Storybook component library. I made this as a project to practive on. It features:
+- React (TypeScript) components
+- Vite + Vitest
+
+## Requirements
+- Node 22
+
+## To run
+- npm install
+- npm run storybook
+
+
---
+++ src/stories/button/button.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import '@testing-library/jest-dom';
+import { Button } from './Button';
+
+const buttonLabel = "Click Me"
+
+describe('Button', () => {
+    it('renders the button with provided text', () => {
+        render(<Button label={buttonLabel}/>);
+        expect(screen.getByRole('button', { name: /click me/i })).toBeInTheDocument();
+    });
+
+    it('calls onClick handler when clicked', () => {
+        const handleClick = vi.fn();
+        render(<Button label={buttonLabel} />);
+        fireEvent.click(screen.getByRole('button'));
+        expect(handleClick).toHaveBeenCalledTimes(1);
+    });
+
+    it('is disabled when disabled prop is true', () => {
+        render(<Button label={buttonLabel} />);
+        expect(screen.getByRole('button')).toBeDisabled();
+    });
+});
